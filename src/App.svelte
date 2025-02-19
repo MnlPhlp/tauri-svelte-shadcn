a1863d6f8ceab510d78bd7dc5872ba7fc5e1702f
MnlPhlp <online@philipp-manuel.de>
2025-02-19 18:34:20 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import DarkMode from "@/components/DarkMode.svelte";
+  import Greet from "@/components/Greet.svelte";
+  import { ModeWatcher } from "mode-watcher";
+</script>
+
+<ModeWatcher />
+<div class="h-full">
+  <main class="container h-full">
+    <DarkMode class="absolute right-3 top-3" />
+    <div class="w-full h-full flex items-center">
+      <Greet />
+    </div>
+  </main>
+</div>
---
+++ src/lib/components/Greet.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { Button } from "./ui/button";
+    import { Input } from "./ui/input";
+    import { commands } from "@/bindings";
+
+    let name = $state("");
+    let greetMsg = $state("");
+
+    async function greet() {
+        greetMsg = await commands.greet(name);
+    }
+
+    function keypress(event: KeyboardEvent) {
+        if (event.key === "Enter") greet();
+    }
+</script>
+
+<div class="col">
+    <div class="row">
+        <Input
+            id="greet-input"
+            placeholder="Enter a name..."
+            onkeypress={keypress}
+            bind:value={name}
+        />
+        <Button onclick={greet}>Greet</Button>
+    </div>
+    <p class="mt-2">
+        {greetMsg}
+    </p>
+</div>
